901b36ab412a0d91d243a69d08c097cf9baea06c
bcmbf14 <bcmbf14@gmail.com>
2020-04-07 07:14:23 +0900

    step6

--- SlackLogin/ViewController.swift
+++ SlackLogin/ViewController.swift
@@ -47,6 +47,23 @@ extension ViewController: UITextFieldDelegate {
             guard string.rangeOfCharacter(from: charSet) == nil else { return false }
         }
         
+        let finalText = NSMutableString(string: textField.text ?? "")
+        finalText.replaceCharacters(in: range, with: string)
+        
+        let font = textField.font ?? UIFont.systemFont(ofSize: 16)
+        
+        let dict = [NSAttributedString.Key.font: font]
+        
+        let width = finalText.size(withAttributes: dict).width
+        
+        placeholderLeadingConstraint.constant = width
+        
+        if finalText.length == 0 {
+            placeholderLabel.text = "workspace-url.slack.com"
+        }else{
+            placeholderLabel.text = ".slack.com"
+        }
+        
         return true
     }
     
