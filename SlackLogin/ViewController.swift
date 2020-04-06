f5b9a86beb2f64f4b893acdd0d9860e2c21414e9
bcmbf14 <bcmbf14@gmail.com>
2020-04-07 06:59:21 +0900

    step4

--- SlackLogin/ViewController.swift
+++ SlackLogin/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var placeholderLabel: UILabel!
+    
+    @IBOutlet weak var placeholderLeadingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("commit test")
+        
     }
 
     
