40db81b9a93726fc6b90fe11a3073fa9cab41528
bcmbf14 <bcmbf14@gmail.com>
2020-04-07 07:06:36 +0900

    step5

--- SlackLogin/ViewController.swift
+++ SlackLogin/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let charSet:CharacterSet = {
+        var cs = CharacterSet.lowercaseLetters
+        cs.insert(charactersIn: "0123456789")
+        cs.insert(charactersIn: "-")
+        return cs.inverted
+    }()
 
     @IBOutlet weak var urlField: UITextField!
     @IBOutlet weak var placeholderLabel: UILabel!
-    
     @IBOutlet weak var placeholderLeadingConstraint: NSLayoutConstraint!
-    
     @IBOutlet weak var nextButton: UIButton!
     
     
@@ -28,3 +33,21 @@ class ViewController: UIViewController {
     
 }
 
+
+
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        //range 편집된범위
+        
+        //입력모드에서만 실행
+        if string.count > 0 {
+            //문자열에서 캐릭터셋에 포함된 문자를 검색한다.
+            //포함되었으면 해당범위를 리턴, 아니면 닐을 리턴
+            //가드문을 썼으므로 charSet만 입력이 가능하도록 강제 세팅
+            guard string.rangeOfCharacter(from: charSet) == nil else { return false }
+        }
+        
+        return true
+    }
+    
+}
