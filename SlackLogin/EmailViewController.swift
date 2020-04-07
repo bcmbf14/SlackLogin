c5a4e2233521f20eb9bfb3cccdd92a3798aaca7b
bcmbf14 <bcmbf14@gmail.com>
2020-04-08 07:12:26 +0900

    step11

--- SlackLogin/EmailViewController.swift
+++ SlackLogin/EmailViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class EmailViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var placeholderLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var titleLabelBottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func movePrevious(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
     }
-    */
+    
 
 }
